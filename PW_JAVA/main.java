71ec2cbfa93327c66226b38f9f21e5692c9c4719
ashishsharma2885 <111562868+ashishsharma2885@users.noreply.github.com>
2023-07-10 17:49:41 +0530

    Add files via upload

---
+++ PW_JAVA/main.java
@@ -0,0 +1,27 @@
+public class main {
+    public static void main(String[] args){
+        // decalre variable a and b 
+        int a = 20, b=10;
+        int result;
+      
+        // addition operator
+        result=a+b;
+        System.out.println(result);
+
+        // Substraction operator
+        System.out.println(a-b);
+
+        // we can directly perform substraction in print statement,no need to
+        // use result variable here
+
+        // multipication operator
+        System.out.println(a * b);
+
+        // divide operator
+        System.out.println(a / b);
+
+        // module operator
+        System.out.println(a % b);
+
+    }
+}
---
+++ PW_JAVA/main0.java
@@ -0,0 +1,14 @@
+public class main0 {
+    public static void main(String[] args){
+        int a = 5,  b = 6;
+        int c = a++; // post increment 
+        int d = ++a; //pre increment 
+        int e = b--; // post decrement
+        int f = -b; // pre decrement
+
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
